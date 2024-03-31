c9659450583694ff52f0fec09d7311575cd8a04d
nikit <nikita_eremeev.95@mail.ru>
2024-03-31 16:57:50 +0300

    dev commit

--- app/src/main/java/com/tz/fooddelivery/data/api/MealsApi.kt
+++ app/src/main/java/com/tz/fooddelivery/data/api/MealsApi.kt
@@ -1,4 +1,9 @@
 package com.tz.fooddelivery.data.api
 
+import com.tz.fooddelivery.data.dto.MealsResponse
+import retrofit2.http.GET
+
 interface MealsApi {
+    @GET("search.php?s")
+    suspend fun getMeals(): MealsResponse
 }
