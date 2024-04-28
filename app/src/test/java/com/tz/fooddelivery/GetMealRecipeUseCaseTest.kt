7bd9cefc7730d8a7f8f11b3952ef48bf280ae77f
nikit <nikita_eremeev.95@mail.ru>
2024-04-28 13:09:18 +0300

    recipes use case test

---
+++ app/src/test/java/com/tz/fooddelivery/GetMealRecipeUseCaseTest.kt
@@ -0,0 +1,66 @@
+package com.tz.fooddelivery
+
+import com.tz.fooddelivery.domain.models.MealRecipe
+import com.tz.fooddelivery.domain.repository.RecipeRepository
+import com.tz.fooddelivery.domain.use_cases.GetMealRecipeUseCase
+import com.tz.fooddelivery.domain.use_cases.GetTranslatedTextUseCase
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+
+class GetMealRecipeUseCaseTest {
+    private lateinit var recipeRepository: RecipeRepository
+    private lateinit var getTranslatedTextUseCase: GetTranslatedTextUseCase
+    private lateinit var getMealRecipeUseCaseTest: GetMealRecipeUseCase
+
+    @Before
+    fun setup(){
+        recipeRepository = mock(RecipeRepository::class.java)
+        getTranslatedTextUseCase = mock(GetTranslatedTextUseCase::class.java)
+        getMealRecipeUseCaseTest = GetMealRecipeUseCase(recipeRepository, getTranslatedTextUseCase)
+    }
+
+    @Test
+    fun `getMealRecipe returns translated recipe`() = runBlocking {
+        val mealId = "1"
+        val mealRecipe = MealRecipe(
+            idMeal = "1",
+            mealTitle = "beef",
+            mealCategory = "meat",
+            area = "USA",
+            mealRecipe = "recipe",
+            mealImage = "image_url",
+            strYoutube = "youtube_url",
+            ingredients = "ingredients"
+        )
+        val translatedMealRecipe = MealRecipe(
+            idMeal = "1",
+            mealTitle = "говядина",
+            mealCategory = "meat",
+            area = "USA",
+            mealRecipe = "рецепт",
+            mealImage = "image_url",
+            strYoutube = "youtube_url",
+            ingredients = "ингредиенты"
+        )
+
+        `when`(recipeRepository.getMealRecipe(mealId)).thenReturn(mealRecipe)
+        `when`(getTranslatedTextUseCase(mealRecipe.mealTitle)).thenReturn(translatedMealRecipe.mealTitle)
+        `when`(getTranslatedTextUseCase(mealRecipe.mealRecipe)).thenReturn(translatedMealRecipe.mealRecipe)
+        `when`(getTranslatedTextUseCase(mealRecipe.ingredients)).thenReturn(translatedMealRecipe.ingredients)
+
+        val result = getMealRecipeUseCaseTest.getMealRecipe(mealId)
+
+        verify(recipeRepository).getMealRecipe(mealId)
+        verify(getTranslatedTextUseCase).invoke(mealRecipe.mealTitle)
+        verify(getTranslatedTextUseCase).invoke(mealRecipe.mealRecipe)
+        verify(getTranslatedTextUseCase).invoke(mealRecipe.ingredients)
+        assertEquals(translatedMealRecipe, result)
+    }
+
+
+}
