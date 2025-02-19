c067e1d38dad374f46937499765ac64285592350
Nikita Eremeev <nikita.eremeev.1231334@gmail.com>
2025-02-19 23:54:15 +0300

    dependency versions update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,14 +43,14 @@ android {
 dependencies {
 
     //Room
-    implementation ("androidx.room:room-runtime:2.5.2")
-    implementation ("androidx.room:room-ktx:2.5.2")
-    kapt ("androidx.room:room-compiler:2.5.2")
+    implementation ("androidx.room:room-runtime:2.6.1")
+    implementation ("androidx.room:room-ktx:2.6.1")
+    kapt ("androidx.room:room-compiler:2.6.1")
 
     //Hilt
     implementation ("com.google.dagger:hilt-android:2.51.1")
     implementation ("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
     kapt ("com.google.dagger:hilt-compiler:2.51.1")
 
     //Retrofit
@@ -58,41 +58,41 @@ dependencies {
     implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     //OkHttp
-    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation ("com.squareup.okio:okio:3.0.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.squareup.okio:okio:3.8.0")
 
     //Gson
-    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("com.google.code.gson:gson:2.11.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     //Glide
-    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     //Coroutine
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
     //Livecycle
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     //Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.1")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.7.1")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.8.7")
 
     //JUnitTest
     testImplementation ("junit:junit:4.13.2")
-    testImplementation ("org.mockito:mockito-core:3.12.4")
-    testImplementation ("org.assertj:assertj-core:3.20.2")
+    testImplementation ("org.mockito:mockito-core:5.2.0")
+    testImplementation ("org.assertj:assertj-core:3.24.2")
 
-    kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.4.2")
-    implementation ("androidx.recyclerview:recyclerview:1.3.1")
+    kapt ("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.5.0")
+    implementation ("androidx.recyclerview:recyclerview:1.4.0")
     implementation ("com.github.javafaker:javafaker:1.0.2")
-    implementation ("androidx.core:core-ktx:1.10.1")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.9.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.core:core-ktx:1.15.0")
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.2.0")
     testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.6.1")
 }
